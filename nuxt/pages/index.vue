3c7b0d6f1b2644814ab9025fa9377efa691ae5be
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-03-07 18:51:43 +0900

    simplify

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -33,26 +33,4 @@ export default Vue.extend({
 .container {
   min-height: 100vh;
 }
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
