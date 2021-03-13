350c8b10c7b8b9a16e01693ac66f84272eed95eb
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-03-14 00:53:02 +0900

    modify entity

--- golang/models/todo.go
+++ golang/models/todo.go
@@ -1,8 +1,11 @@
 package models
 
+import "time"
+
 type Todo struct {
-	ID    int    `gorm:"primary_key;not null"       json:"id"`
-	Name  string `gorm:"type:varchar(200);not null" json:"name"`
-	Memo  string `gorm:"type:varchar(400)"          json:"memo"`
-	State int    `gorm:"not null"                   json:"state"`
+	UUID      string    `gorm:"type:varchar(36); primary_key; not null"       json:"uuid"`
+	Title     string    `gorm:"type:varchar(200);not null" json:"title"`
+	State     bool      `gorm:"not null"                   json:"state"`
+	CreatedAt time.Time `gorm:"not null" json:"created_at" sql:"DEFAULT:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time `gorm:"not null" json:"created_at" sql:"DEFAULT:CURRENT_TIMESTAMP"`
 }
