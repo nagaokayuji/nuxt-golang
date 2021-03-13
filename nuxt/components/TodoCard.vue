626084249d893446605a4a5d2c3a482d14896853
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-03-14 06:18:03 +0900

    feat: format date

--- nuxt/components/TodoCard.vue
+++ nuxt/components/TodoCard.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
 div
-  p {{todo.title}} {{todo.deadline}} {{todo.state}}
+  p {{todo.title}} {{todo.deadline | dateFormat}} {{todo.state}}
 
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from "vue";
 import axios from "axios";
-
+import moment from "moment";
 interface TodoResponse {
   uuid: string;
   title: string;
@@ -18,6 +18,11 @@ interface TodoResponse {
 export default Vue.extend({
   props: {
     todo: Object as PropType<TodoResponse>,
+  },
+  filters: {
+    dateFormat(date: Date): string {
+      return moment(date).format("YYYY/MM/DD");
+    },
   },
   methods: {},
 });
