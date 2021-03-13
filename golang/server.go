fe095255d2c24f2f881690b748b6cfb8a5188ba3
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-03-13 20:01:39 +0900

    update server

--- golang/server.go
+++ golang/server.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"app/controllers"
+
+	"app/models"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
@@ -10,11 +15,22 @@ func main() {
 }
 
 func server() {
+	db := models.SetupDB()
+	db.AutoMigrate(&models.Todo{})
 	r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+	})
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "hello world from api",
 		})
 	})
+
+	// 全件
+	r.GET("/todos", controllers.GetAllTodos)
+
 	r.Run(":8080")
 }
