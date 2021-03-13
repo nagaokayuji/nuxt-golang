98529f48dfa9d7ff3ca061afbc86b2a10a7fd3a2
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-03-14 02:16:21 +0900

    ✨ add: controller methods

--- golang/controllers/todoController.go
+++ golang/controllers/todoController.go
@@ -1,17 +1,35 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"app/models"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
+	"github.com/google/uuid"
 )
 
+// 全件取得
 func GetAllTodos(c *gin.Context) {
-	db := c.MustGet("db").(*gorm.DB)
 	var todos []models.Todo
-	db.Find(&todos)
-	c.JSON(http.StatusOK, gin.H{"data": todos})
+	models.DB.Find(&todos)
+	c.JSON(http.StatusOK, todos)
+}
+
+// UUIDから1件取得
+func GetTodo(c *gin.Context) {
+	uuid := c.Params.ByName("uuid")
+	var todo models.Todo
+	if err := models.DB.Where("uuid = ?", uuid).First(&todo).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(http.StatusOK, todo)
+	}
+}
+
+func CreateTodo(c *gin.Context) {
+	// TODO not yet implemented
+	fmt.Println(uuid.New().String())
 }
