aaa23f47cb7f3f63478efc0042b962846b94ba71
Yuji Nagaoka <yvay5cqe@gmail.com>
2021-04-03 17:07:40 +0900

    delete: sampleData

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,31 +1,12 @@
 <template lang="pug">
   .container
-    div
-      button(@click="push") push here to send request
-    div ↓↓ response ↓↓
-    p {{sampleData}}
     TodoApp
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import axios from "axios";
 
-export default Vue.extend({
-  data() {
-    return {
-      sampleData: "",
-    };
-  },
-  mounted() {
-    console.log("mounted");
-  },
-  methods: {
-    push(): void {
-      axios.get("/api/").then((response) => (this.sampleData = response.data));
-    },
-  },
-});
+export default Vue.extend();
 </script>
 
 <style lang="scss">
